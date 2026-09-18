dbde0d654716c675f209764f4fc65f46925a602d
rayan <78854667+rayanux@users.noreply.github.com>
2026-09-18 20:49:40 +0100

    Refactor binary search logic in binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,33 +1,25 @@
 database = [1,2,3,4,5,6,7,8,9,10]
 copy = database
-length = len(database)
-start = 0
-midpoint = length//2
 found = False
-
-number = int(input("enter a number: "))
+start = 0
+length = len(database)
+midpoint = length//2
+number = int(input("enter a number you'd like to find: "))
 
 while found == False:
-    if len(database) == 0:
-        print("not found")
-        break
-
-    if number > database[midpoint]:
-        print("discard left")
-        database = database[midpoint:length]
-        midpoint = len(database)//2
-        print(database)
-        print(midpoint)
-
-    elif number < database[midpoint]:
-        print("discard right")
-        database = database[start:midpoint]
-        midpoint = len(database)//2
-        print(database)
-        print(midpoint)
-
-    elif number == database[midpoint]:
-        position = copy.index(number)
-        print(f"{number} was found in position {position}")
-        found = True
-        
+  if number not in database:
+    print("number not found in list")
+    break
+  elif number > database[midpoint]:
+    database = database[midpoint:length]
+    midpoint = len(database)//2
+    print(database)
+  elif number < database[midpoint]:
+    database = database[start:midpoint]
+    midpoint = len(database)//2
+    print(database)
+  elif number == database[midpoint]:
+    position = copy.index(number)
+    print(f"{number} was found in position {position}")
+    break
+    
